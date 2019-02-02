f3bf2d085637438ea7402119cc793b82d4632b07
unknown <willyduguid@gmail.com>
2019-02-02 14:04:47 +1000

    fixed error with sticky nav (in changing display, left space in DOM)

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 var originalNavTop;
 
+var navHeight;
+var aboutTop;
+var projectTop;
+var contactTop;
+
+var stuck = false;
 
 $(document).ready(function(){
+    originalNavTop = parseInt($('nav').offset().top);
+    calculateAllTop();
 
-    originalNavTop = $('nav').offset().top;
-
-    //top nav buttons
+    /*top nav buttons
     $('#graphics-nav, #web-nav').on('click', function(){
         var id = $(this).attr('id');
-        console.log(id);
 
         scrollWindow('#content-container');
 
         if($(this).hasClass('active')){
             $('.example').css('display','inline');
-            $(this).removeClass('active');
+            //$(this).removeClass('active');
         }
 
         else{
-            //if any of the navs have active class remove underline
+            /*if any of the navs have active class remove underline
             if($('.active').length){
                 console.log('active exists');
                 $('.active').removeClass('active');
@@ -34,46 +39,75 @@ $(document).ready(function(){
                 $('.web').css('display','none');
             }
 
-            $(this).addClass('active');
+            //$(this).addClass('active');
         }
 
         
         
-    })
+    })*/
 
-    $('#contact-nav ').on('click', function(){
-        $(this).addClass('active');
+    $('#about-nav ').on('click', function(){
+        //$(this).addClass('active');
         $('.example').css('display','inline');
         scrollWindow('#about');
     })
+
     $('#projects-nav').on('click', function(){
-        $(this).addClass('active');
+        //$(this).addClass('active');
         $('.example').css('display','inline');
         scrollWindow('#content-container');
     })
 
     $('#back-home.home ').on('click', function(){
-        $('.active').removeClass('active');
+        //$('.active').removeClass('active');
         $('.example').css('display','inline');
         scrollWindow('body');
     })
+
+    //example link navigation
     $('#content-container .example > div').on('click', function(){
         var targetLocation = $(this).attr('href');
-        console.log(targetLocation);
         window.location.href = targetLocation;
     })
 
-    //ghetto sticky
+    
     $(window).on('scroll', function(){
         var windowTop = $(window).scrollTop();
-        
 
+        calculateAllTop();
+
+        if(windowTop < aboutTop-4){
+            $('.active').removeClass('active');
+        }
+        else if(windowTop >= aboutTop -4 && windowTop < projectTop){
+            $('.active').removeClass('active');
+            $('#about-nav').addClass('active');
+        }
+        else if(windowTop >= projectTop && windowTop < contactTop){
+            $('.active').removeClass('active');
+            $('#projects-nav').addClass('active');
+        }
+        else if(windowTop >= contactTop){
+            //will be used when site map is added
+        }
+
+
+        //ghetto sticky
         if(windowTop >= originalNavTop){
-            console.log('stick');
+            console.log('sticky');
+            stuck = true;
+
+            $('#align').css('padding-top',navHeight);
+
             $('nav').css('position', 'fixed').css('top', '0');
             $('#back-home.home').css('display', 'inline').css('position', 'fixed');
         }
         else{
+            console.log('UNsticky');
+            stuck = false;
+
+            $('#align').css('padding-top','0');
+
             $('nav').css('position', 'static');
             $('#back-home.home').css('display', 'none').css('position', 'absolute');
         }
@@ -83,9 +117,18 @@ $(document).ready(function(){
 })
 
 function scrollWindow (target){
-    var navHeight = $('nav').height();
 
     $(':animated').stop();
     $('html, body').animate({scrollTop: $(target).offset().top - navHeight}, 1000);
+}
+
+function calculateAllTop(){
+
+    navHeight = $('nav').height();
+
     
+    aboutTop = parseInt($('#about').offset().top - navHeight);
+    projectTop = parseInt($('#content-container').offset().top - navHeight);
+    contactTop = parseInt($('#contact').offset().top - navHeight);
+
 }
